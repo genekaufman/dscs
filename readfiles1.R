c389c9acf6f6d30d8b8099163a895edbafd4aedb
unknown <gekaufma@LT-US-GEKAUFMA.americas.phoenixcontact.com>
2016-11-16 18:04:23 +0000

    build DF

--- readfiles1.R
+++ readfiles1.R
@@ -1,17 +1,25 @@
+library(caret)
+
 # build more manageable data files
-full_blogs <- "original_data/en_US/en_US.blogs.txt";
-full_new <- "original_data/en_US/en_US.news.txt";
-full_twitter <- "original_data/en_US/en_US.twitter.txt";
+full_blogs <- "data/en_US/en_US.blogs.txt";
+full_new <- "data/en_US/en_US.news.txt";
+full_twitter <- "data/en_US/en_US.twitter.txt";
 
 smp_size <- 100;
 
 set.seed(42)
-#full_blogs_data <- read.table(full_blogs)
+if (!exists("full_blogs_data")) {
+  full_blogs_data <- readLines(full_blogs)
+  full_blogs_data <-as.data.frame(full_blogs_data)
+
+}
+rndBlogsIndex <- sample(1:nrow(full_blogs_data),1000)
+blogs_data_1000 <- full_blogs_data[rndBlogsIndex,]
+
+#con <- file(full_blogs, "r")
+#aa <- readLines(con, smp_size) ## Read the next line of text
+##readLines(con, 5) ## Read in the next 5 lines of text
+#close(con) ## It's important to close the connection when you are done
 
 
-con <- file(full_blogs, "r")
-#aa< - readLines(con, smp_size) ## Read the first line of text
-aa <- readLines(con, smp_size) ## Read the next line of text
-#readLines(con, 5) ## Read in the next 5 lines of text
-close(con) ## It's important to close the connection when you are done
 
