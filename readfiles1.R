aee5e3ba5815ce74d82919e4571fc0c732716230
Gene Kaufman <genekaufman@gmail.com>
2016-11-17 18:14:13 +0000

    sample data files built

--- readfiles1.R
+++ readfiles1.R
@@ -1,25 +1,65 @@
 library(caret)
 
-# build more manageable data files
-full_blogs <- "data/en_US/en_US.blogs.txt";
-full_new <- "data/en_US/en_US.news.txt";
-full_twitter <- "data/en_US/en_US.twitter.txt";
+baseDataDir <- "data/en_US/";
 
-smp_size <- 100;
+readDataFile <- function(dataFileName) {
+  tmpFile <- readLines(dataFileName)
+  tmpFile <-as.data.frame(tmpFile)
+  tmpFile
+}
 
-set.seed(42)
-if (!exists("full_blogs_data")) {
-  full_blogs_data <- readLines(full_blogs)
-  full_blogs_data <-as.data.frame(full_blogs_data)
+buildOutputFile <- function(seed2use,smpSize2use,dataObjName,baseFileName) {
+  tmpOutFile <- paste0(baseDataDir,baseFileName,"_smp_",smpSize2use,"_seed",seed2use,".txt");
+  tmpInFile  <- paste0(baseDataDir,baseFileName,".txt");
+  if (!file.exists(tmpOutFile)) {
+    set.seed(seed2use);
+    tmpIndex <- sample(1:nrow(dataObjName),smpSize2use);
+    tmpSampleData <- dataObjName[tmpIndex,]
+    write.table(tmpSampleData,file=tmpOutFile,quote=FALSE,row.names = FALSE,col.names = FALSE);
+  }
 
 }
-rndBlogsIndex <- sample(1:nrow(full_blogs_data),1000)
-blogs_data_1000 <- full_blogs_data[rndBlogsIndex,]
 
-#con <- file(full_blogs, "r")
-#aa <- readLines(con, smp_size) ## Read the next line of text
-##readLines(con, 5) ## Read in the next 5 lines of text
-#close(con) ## It's important to close the connection when you are done
+buildFullSet <- function(seed,smplsize,blogData,newsData,twitterData) {
+  thisSeed = seed;
+  thisSampSize=smplsize;
+  buildOutputFile(seed2use=thisSeed,smpSize2use = thisSampSize,dataObjName = blogData,baseFileName = "en_US.blogs")
+  buildOutputFile(seed2use=thisSeed,smpSize2use = thisSampSize,dataObjName = newsData,baseFileName = "en_US.news")
+  buildOutputFile(seed2use=thisSeed,smpSize2use = thisSampSize,dataObjName = twitterData,baseFileName = "en_US.twitter")
 
+}
 
+# build more manageable data files
+full_blogs_infile <- paste0(baseDataDir, "en_US.blogs.txt");
+full_news_infile <- paste0(baseDataDir, "en_US.news.txt");
+full_twitter_infile <- paste0(baseDataDir, "en_US.twitter.txt");
+
+start.time <- Sys.time()
+
+if (!exists("full_blogs_data")) {
+  full_blogs_data <- readDataFile(full_blogs_infile)
+}
+if (!exists("full_news_data")) {
+  full_news_data <- readDataFile(full_news_infile)
+}
+if (!exists("full_twitter_data")) {
+  full_twitter_data <- readDataFile(full_twitter_infile)
+}
+
+######### Build 100-line files #####
+buildFullSet(seed=42,smplsize = 100,blogData = full_blogs_data,newsData = full_news_data, twitterData = full_twitter_data);
+buildFullSet(seed=42,smplsize = 1000,blogData = full_blogs_data,newsData = full_news_data, twitterData = full_twitter_data);
+buildFullSet(seed=42,smplsize = 8000,blogData = full_blogs_data,newsData = full_news_data, twitterData = full_twitter_data);
+buildFullSet(seed=68,smplsize = 1000,blogData = full_blogs_data,newsData = full_news_data, twitterData = full_twitter_data);
+buildFullSet(seed=1986,smplsize = 5000,blogData = full_blogs_data,newsData = full_news_data, twitterData = full_twitter_data);
+
+end.time <- Sys.time()
+time.taken <- paste( as.numeric(round(end.time - start.time,3)*1000), "milliseconds")
+time.taken
+
+##thisSeed = 42;
+##thisSampSize=100;
+##buildOutputFile(seed2use=thisSeed,smpSize2use = thisSampSize,dataObjName = full_blogs_data,baseFileName = "en_US.blogs")
+##buildOutputFile(seed2use=thisSeed,smpSize2use = thisSampSize,dataObjName = full_news_infile,baseFileName = "en_US.news")
+##buildOutputFile(seed2use=thisSeed,smpSize2use = thisSampSize,dataObjName = full_twitter_infile,baseFileName = "en_US.twitter")
 
