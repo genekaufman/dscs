8f0d190056238648bfbfd3ab9bab11ff0f8c1b3a
Gene Kaufman <genekaufman@gmail.com>
2016-11-19 14:53:20 -0500

    added rds support

--- readfiles1.R
+++ readfiles1.R
@@ -38,15 +38,32 @@ full_twitter_infile <- paste0(baseDataDir, "en_US.twitter.txt");
 start.time <- Sys.time()
 
 if (!exists("full_blogs_data")) {
-  full_blogs_data <- readDataFile(full_blogs_infile)
+  full_blogs_datafile <- paste0(baseDataDir, "full_blogs_data.Rds");
+  if (file.exists(full_blogs_datafile)) {
+    full_blogs_data <- readRDS(full_blogs_datafile)
+  } else {
+    full_blogs_data <- readDataFile(full_blogs_infile);
+    saveRDS(full_blogs_data,file=full_blogs_datafile);
+  }
 }
 if (!exists("full_news_data")) {
-  full_news_data <- readDataFile(full_news_infile)
+  full_news_datafile <- paste0(baseDataDir, "full_news_data.Rds");
+  if (file.exists(full_news_datafile)) {
+    full_news_data <- readRDS(full_news_datafile)
+  } else {
+    full_news_data <- readDataFile(full_news_infile);
+    saveRDS(full_news_data,file=full_news_datafile);
+  }
 }
 if (!exists("full_twitter_data")) {
-  full_twitter_data <- readDataFile(full_twitter_infile)
+  full_twitter_datafile <- paste0(baseDataDir, "full_twitter_data.Rds");
+  if (file.exists(full_twitter_datafile)) {
+    full_twitter_data <- readRDS(full_twitter_datafile)
+  } else {
+    full_twitter_data <- readDataFile(full_twitter_infile);
+    saveRDS(full_twitter_data,file=full_twitter_datafile);
+  }
 }
-
 ######### Build 100-line files #####
 doFiles <- FALSE;
 if (doFiles) {
