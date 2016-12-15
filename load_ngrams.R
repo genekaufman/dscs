9cef522c87a84bf0f4ff37b2ae0b739d27ad0711
Gene Kaufman <genekaufman@gmail.com>
2016-12-15 11:23:32 -0500

    load ngrams

---
+++ load_ngrams.R
@@ -0,0 +1,26 @@
+if (!exists("betterMessage")) {
+  betterMessage <- function(strIn) {
+    message(paste0('[',date(),'] ',strIn));
+
+  }
+}
+baseDataDir <- "data/en_US/";
+
+samp_perc <- 0.5;
+MaxN_Files <- 2;
+
+
+for(n in 1:MaxN_Files){
+  thisRDS <- paste0("n", n,"_s",samp_perc);
+  thisRDSfile <- paste0(thisRDS, ".Rds");
+  thisRDSfilePath <- paste0(baseDataDir, thisRDSfile);
+  betterMessage(paste('### Looking for: ', thisRDSfile));
+  if (file.exists(thisRDSfilePath)) {
+    betterMessage(paste(thisRDSfilePath, " exists, loading"));
+    thisRDS <- readRDS(thisRDSfilePath);
+  } else {
+    betterMessage(paste(thisRDSfilePath, " doesn't exist, skipping"));
+  }
+}
+
+betterMessage("## ngrams ready!");
