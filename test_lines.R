bd69d2ef0d95e45aeb0652e0117228b7fdd21bf5
Gene Kaufman <genekaufman@gmail.com>
2016-12-28 22:15:45 -0500

    test sentences

---
+++ test_lines.R
@@ -0,0 +1,22 @@
+getBestTermSBO("Every inch of you is perfect from the bottom to the");
+getBestTermSBO("Guy at my table's wife got up to go to the bathroom and I asked about dessert and he started telling me about his");
+getBestTermSBO("I can't deal with unsymetrical things. I can't even hold an uneven number of bags of groceries in each");
+getBestTermSBO("I like how the same people are in almost all of Adam Sandler's");
+getBestTermSBO("I'd give anything to see arctic monkeys this");
+getBestTermSBO("I'd just like all of these questions answered, a presentation of evidence, and a jury to settle the");
+getBestTermSBO("I'm thankful my childhood was filled with imagination and bruises from playing");
+getBestTermSBO("Talking to your mom has the same effect as a hug and helps reduce your");
+getBestTermSBO("When you breathe, I want to be the air for you. I'll be there for you, I'd live and I'd");
+getBestTermSBO("When you were in Holland you were like 1 inch away from me but you hadn't time to take a");
+
+getBestTermSBO("Ohhhhh #PointBreak is on tomorrow. Love that film and haven't seen it in quite some");
+getBestTermSBO("Well I'm pretty sure my granny has some old bagpipes in her garage I'll dust them off and be on my");
+getBestTermSBO("You're the reason why I smile everyday. Can you follow me please? It would mean the");
+getBestTermSBO("After the ice bucket challenge Louis will push his long wet hair out of his eyes with his little");
+getBestTermSBO("Be grateful for the good times and keep the faith during the");
+getBestTermSBO("Go on a romantic date at the");
+getBestTermSBO("Hey sunshine, can you follow me and make me the");
+getBestTermSBO("If this isn't the cutest thing you've ever seen, then you must be");
+getBestTermSBO("The guy in front of me just bought a pound of bacon, a bouquet, and a case of");
+getBestTermSBO("Very early observations on the Bills game: Offense still struggling but the");
+
